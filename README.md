4e9f74446bbc45ca34f8dc3c77b36aebc0209272
Miguel Torres <miguelmariatorresrojas@gmail.com>
2016-12-23 13:40:15 +0100

    finishin css variables and startin webfonts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotcss-2016-summary
+
+Summary of the [dotCSS 2016 conference](https://www.dotcss.io) 
+
+See it online: https://mmtr.github.io/dotcss-2016-summary/
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,27 +3,52 @@
 Reveal.initialize({
   controls: false,
   history: true,
-  transition: 'convex'
+  transition: 'convex',
+  keyboard: {
+    37: 'prev',
+    39: 'next',
+  }
 });
 
-Reveal.addEventListener( 'ready', function( event ) {
-    hljs.initHighlighting()
-} );
+Reveal.addEventListener('ready', event => hljs.initHighlighting());
 
 Reveal.addEventListener('fragmentshown', event => {
-  let example = parseInt(event.fragment.dataset.example);
-  switch (example) {
-    case 1:
-      document.querySelector(':root').style.setProperty('--color', 'green');
-      break;
-  }
+  let example = event.fragment.dataset.example;
+  window[example]('show');
 });
 
 Reveal.addEventListener('fragmenthidden', event => {
-  let example = parseInt(event.fragment.dataset.example);
-  switch (example) {
-    case 1:
-      document.querySelector(':root').style.setProperty('--color', 'red');
-      break;
-  }
+  let example = event.fragment.dataset.example;
+  window[example]('hide');
 });
+
+window.cssVariables1 = function cssVariables1(action) {
+  let color = action === 'show' ? 'green' : 'red';
+  document.querySelector(':root').style.setProperty('--color', color);
+};
+
+window.cssVariables2 = function cssVariables2() {
+  let element = document.querySelector('.js-text');
+  let maxScroll = element.scrollHeight - element.clientHeight;
+  let currentScroll = element.scrollTop;
+  let percentageScroll = currentScroll * 100 / maxScroll;
+  document.querySelector('.js-progress-bar').style.setProperty('--progress', percentageScroll + '%');
+};
+
+window.webfonts1 = function webfonts1(action) {
+  let variantNumeric = action === 'show' ? 'oldstyle-nums' : 'normal';
+  document.querySelector('.js-example-webfonts-1').style.setProperty('font-variant-numeric', variantNumeric);
+};
+
+window.webfonts2 = function webfonts2(action) {
+  let variantNumeric = action === 'show' ? 'tabular-nums' : 'proportional-nums';
+  document.querySelector('.js-example-webfonts-2').style.setProperty('font-variant-numeric', variantNumeric);
+};
+
+window.webfonts3 = function webfonts3(action) {
+  let variantNumeric = action === 'show' ? 'diagonal-fractions' : 'normal';
+  document.querySelector('.js-example-webfonts-3').style.setProperty('font-variant-numeric', variantNumeric);
+};
+
+
+$('.example-css-variables-4 .text').scroll(() => cssVariables2());
